5bf0e6fba6250bd7e16b953d6ebdc55acd084a23
Loris Bazzani <loris.bazzani@dartmouth.edu>
2014-01-20 14:28:31 -0500

    Added support to heatmaps. TODO: unittest heatmap extractor

--- heatextractor.py
+++ heatextractor.py
@@ -35,12 +35,15 @@ class HeatmapExtractorSegm(HeatmapExtractor):
         # Init the list of heamaps
         heamaps     = []
         
+        # Classify the full image without obfuscation
+        caffe_rep_full   = net.predict(image)
+        
         # Perform segmentation
         segm_masks = self.segment.extract(image) # list of segmentation masks
         
         for s in range(np.shape(segm_masks)[0]): # for each segm. mask
         
-            heatmap     = Heatmap() # init heatmap     
+            heatmap     = Heatmap(img.shape[0], img.shape[1]) # init heatmap     
 
             segm_mask   = segm_masks[s] # retrieve s-th mask
 
@@ -48,7 +51,8 @@ class HeatmapExtractorSegm(HeatmapExtractor):
             heatmap_loc = np.zeros(np.shape(segm_mask))
             for id_segment in range(np.max(segm_mask)):
                 image_obf  = np.array(image) # copy array
-
+                
+                # obfuscation 
                 if np.shape(image.shape)[0]>2: 
                     image_obf[segm_mask==id_segment,0] = self.network.get_mean_img()[0]
                     image_obf[segm_mask==id_segment,1] = self.network.get_mean_img()[1]
@@ -60,11 +64,13 @@ class HeatmapExtractorSegm(HeatmapExtractor):
                 caffe_rep_obf   = self.network.evaluate(image_obf)
 
                 # Given the class of the image, select the confidence
-                heatmap_loc[segm_mask==id_segment] = 1-caffe_rep_obf[lab_id]    
+                #confidence = 1-caffe_rep_obf[lab_id]     ## TODO: test this
+                confidence = caffe_rep_full[lab_id] - caffe_rep_obf[lab_id]
+                
+                # update the heatmap ## TODO: test normalize vs. unnormalized
+                heatmap.add_val_segment(confidence, id_segment, segm_mask) 
 
-            #heatmap.set(heatmap_loc) # set the heatmaps 
-
-            #heamaps.append(heatmap) # append the heatmap to the list
+            heamaps.append(heatmap) # append the heatmap to the list
                     
         return heamaps
         
