889cfc8db062cc9ef593bd577eeccaa775b180a4
Loris Bazzani <loris.bazzani@dartmouth.edu>
2014-01-22 10:18:20 -0500

    Unit test heatmap caffe+felz segmentation done and tested.

--- heatextractor_test.py
+++ heatextractor_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage.data import imread
+from skimage.transform import resize
 import unittest
 from network import *
 from configuration import *
@@ -13,7 +14,7 @@ class HeatmapExtractorSegmCaffe(unittest.TestCase):
                                 self.conf.ilsvrc2012_caffe_model,\
                                 self.conf.ilsvrc2012_caffe_wnids_words,\
                                 self.conf.ilsvrc2012_caffe_avg_image)
-        self.segm = ImgSegmFelzen(sigmas=[0.1, 0.4], mins=[40], scales=[100, 300])  
+        self.segm = ImgSegmFelzen(sigmas=[0.4, 0.6], mins=[40], scales=[100, 300])  
         self.heatext = HeatmapExtractorSegm(self.net, self.segm)
         
     def tearDown(self):
@@ -22,11 +23,21 @@ class HeatmapExtractorSegmCaffe(unittest.TestCase):
         self.segm = None   
         self.heatext = None
 
-    def test_extract(self):
+    def test_extract_img(self):
         img = imread('ILSVRC2012_val_00000001_n01751748.JPEG')
+        img = resize(img, (100,100)) # resize fo faster computation
+        print 'Heatmap computation using segmentation may take a while (around 30-40 seconds)...'
         heatmaps = self.heatext.extract(img,'n01751748')
-        self.assertEqual(heatmaps, [])
-
+        self.assertEqual(np.shape(heatmaps)[0], 4)
+        self.assertAlmostEqual(np.sum(heatmaps[0].get_values()), 7.226420715, places=5)
+        self.assertAlmostEqual(heatmaps[0].get_values()[50,50], 0.001178693, places=5)
+        self.assertAlmostEqual(np.sum(heatmaps[1].get_values()), 0.421182818, places=5)
+        self.assertAlmostEqual(heatmaps[1].get_values()[70,70], 0.0, places=5)
+        self.assertAlmostEqual(np.sum(heatmaps[2].get_values()), 6.376812301, places=5)
+        self.assertAlmostEqual(heatmaps[2].get_values()[50,50], 0.001235786, places=5)
+        self.assertAlmostEqual(np.sum(heatmaps[3].get_values()), 0.302846163, places=5)
+        self.assertAlmostEqual(heatmaps[3].get_values()[50,50], 8.763283776e-05, places=5)
+        
 #=============================================================================
 
 # TODO here test slic segmentation, whenever it is implemented
