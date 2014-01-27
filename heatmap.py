285cb7f8406afe56ef2bdecf3da21fb7f2e7607a
Alessandro Bergamo <aleb@cs.dartmouth.edu>
2014-01-27 14:45:37 -0500

    more efficient implementation of set_val_segment

--- heatmap.py
+++ heatmap.py
@@ -10,6 +10,7 @@ class Heatmap:
         """
         self.vals_ = np.zeros((height, width), np.float64)
         self.counts_ = np.zeros((height, width), np.int32)
+        self.segment_map_ = None
 
     def add_val_rect(self, val, x, y, width = 1, height = 1, \
                      area_normalization = True):
@@ -24,27 +25,42 @@ class Heatmap:
                 self.vals_[y2, x2] += val / float(normalization_factor)
                 self.counts_[y2, x2] += 1
 
-    def add_val_segment(self, val, segment_id, segment_map, \
-                        area_normalization = True):
+    def set_segment_map(self, segment_map):
         """
-        Add val on the segment_id \in {0, ..., num_segments-1}.
-        segment_map is a numpy.int32 of the same size as the heatmap, where
-        the segment_map[i, j] value is the segment-id for that (i, j)-location
+        Set the current segmentation map to use.
+        segment_map can be:
+        - numpy.int32 of the same size as the heatmap, where
+          the segment_map[i, j] value is the segment-id for that (i, j)-location
+        - list of num_segments lists, where the i-th list contains
+          the (x, y) locations for the i-th segment.
         """
-        assert isinstance(segment_map, np.ndarray)
+        if isinstance(segment_map, np.ndarray):
+            # calculate the number of segments present in this map, and
+            # create the segment_map_, as a list of lists
+            num_segments = np.max(segment_map) + 1
+            assert num_segments > 0
+            # create the segment_map_, as a list of lists
+            self.segment_map_ = [[] for i in range(num_segments)]
+            for y in range(segment_map.shape[0]):
+                for x in range(segment_map.shape[1]):
+                    self.segment_map_[segment_map[y, x]].append( (x, y) )
+        elif isinstance(segment_map, list):
+            self.segment_map_ = segment_map
+        else:
+            raise ValueError('segment_map type not supported')
+
+    def add_val_segment(self, val, segment_id, area_normalization = True):
+        """
+        Add val on the segment_id \in {0, ..., num_segments-1}, using the
+        current segmentation map.
+        """
+        assert self.segment_map_ != None
         normalization_factor = 1.0
         if area_normalization:
-            area_region = 0
-            for y2 in range(self.vals_.shape[0]):
-                for x2 in range(self.vals_.shape[1]):
-                    if segment_map[y2, x2] == segment_id:
-                        area_region += 1
-            normalization_factor = float(area_region)
-        for y2 in range(self.vals_.shape[0]):
-            for x2 in range(self.vals_.shape[1]):
-                if segment_map[y2, x2] == segment_id:
-                    self.vals_[y2, x2] += val / float(normalization_factor)
-                    self.counts_[y2, x2] += 1
+            normalization_factor = float(len(self.segment_map_[segment_id]))
+        for (x, y) in self.segment_map_[segment_id]:
+            self.vals_[y, x] += val / float(normalization_factor)
+            self.counts_[y, x] += 1
 
     def normalize_counts(self):
         """
