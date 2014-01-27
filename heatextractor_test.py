031e5a8f79714be99a70f00dc70894c1ad8dae44
Loris Bazzani <loris.bazzani@dartmouth.edu>
2014-01-27 18:03:23 -0500

    Unit test sliding window + caffe.

--- heatextractor_test.py
+++ heatextractor_test.py
@@ -71,7 +71,39 @@ class HeatmapExtractorBoxCaffe(unittest.TestCase):
         self.assertAlmostEqual(heatmaps[0].get_values()[50,50], 0.009513479, places=5)
         self.assertAlmostEqual(np.sum(heatmaps[1].get_values()), 10.88570486, places=5)
         self.assertAlmostEqual(heatmaps[1].get_values()[70,70], 0.001089485, places=5)
+        
+        
+#=============================================================================
 
+class HeatmapExtractorSlidingCaffe(unittest.TestCase):
+    def setUp(self):
+        self.conf = Configuration()
+        self.net = NetworkCaffe(self.conf.ilsvrc2012_caffe_model_spec,\
+                                self.conf.ilsvrc2012_caffe_model,\
+                                self.conf.ilsvrc2012_caffe_wnids_words,\
+                                self.conf.ilsvrc2012_caffe_avg_image)
+        box_sz = [20, 40]
+        stride = 10
+        self.heatext = HeatmapExtractorSliding(self.net, box_sz, stride, \
+                                               confidence_tech = 'only_obf')
+        
+    def tearDown(self):
+        self.conf = None
+        self.net = None
+        self.heatext = None
+
+    def test_extract_img(self):
+        img = imread('ILSVRC2012_val_00000001_n01751748.JPEG')
+        img = resize(img, (100,100)) # resize fo faster computation
+        img = skimage.img_as_ubyte(img)
+        print 'Heatmap computation using gray box may take a while (around 60-70 seconds)...'
+        heatmaps = self.heatext.extract(img,'n01751748')
+        self.assertEqual(np.shape(heatmaps)[0], 2)
+        self.assertAlmostEqual(np.sum(heatmaps[0].get_values()), 24.98861628, places=5)
+        self.assertAlmostEqual(heatmaps[0].get_values()[50,50], 0.002499311, places=5)
+        self.assertAlmostEqual(np.sum(heatmaps[1].get_values()), 6.227268339, places=5)
+        self.assertAlmostEqual(heatmaps[1].get_values()[70,70], 0.000624565, places=5)
+        
 #=============================================================================
 
 # TODO here test slic segmentation, whenever it is implemented
