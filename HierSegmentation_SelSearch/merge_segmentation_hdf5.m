fdafb05676c6cbfa5e19ea8374b977d083e366c2
Loris Bazzani <lbazzani@anthill.cs.dartmouth.edu>
2014-02-05 10:27:20 -0500

    Add script that merges the segmentation results in a single HDF5 dataset.

--- HierSegmentation_SelSearch/merge_segmentation_hdf5.m
+++ HierSegmentation_SelSearch/merge_segmentation_hdf5.m
@@ -1,9 +1,7 @@
-function Segmentation_ImageNet(dSET_)
-% Run segmentation of [1] on ImageNet
-%    dSET_ = 'val or 'train'
-%   [1] Selective Search for Object Recognition,
-%   J.R.R. Uijlings, K.E.A. van de Sande, T. Gevers, A.W.M. Smeulders, IJCV 2013
-% ---Modified by Loris Bazzani 01/2014 to run on the cluster and ImageNet
+% function merge_segmentation_hdf5(dSET_)
+% This function merges the segmentation results saved by the function 
+% Segmentation_ImageNet in a single HDF5 database.
+
 
 tiny_example = 0; % for debugging
 
@@ -23,11 +21,6 @@ seg_params.sigma = 0.8;
 seg_params.colorTypes = seg_params.colorTypes(1:2); % 'Fast' uses HSV and Lab
 seg_params.simFunctionHandles = seg_params.simFunctionHandles(1); % only the one of the paper
 seg_params.ks = seg_params.ks(1:2);
-
-fprintf('---Parameters setup---\n')
-fprintf([' * k: ', num2str(seg_params.ks), '\n'])
-fprintf(' * min_segm: same as k \n')
-fprintf([' * sigma: ', num2str(seg_params.sigma), '\n'])
 
 
 % Configurations
@@ -67,50 +60,21 @@ switch (hostname)
         run_on_anthill = 0;
 
 end
-% add required libraires
-addpath([rootPath '/utils']);
-addpath(genpath(selectivePath));
-        
-% create the output directories
+
+
+% Check the output directories
 if ~exist(savePath, 'file')
-  mkdir(savePath);
+    error('Results not found. Use Segmentation_ImageNet to compute them.');
 end
 if ~exist([savePath '/val'], 'file')
-  mkdir([savePath '/val']);
+    error('Results not found. Use Segmentation_ImageNet to compute them.');
 end
 if ~exist([savePath '/train'], 'file')
-  mkdir([savePath '/train']);
+    error('Results not found. Use Segmentation_ImageNet to compute them.');
 end
 if ~exist([savePath '/test'], 'file')
-  mkdir([savePath '/test']);
+    error('Results not found. Use Segmentation_ImageNet to compute them.');
 end
-
-if run_on_anthill
-    % Anthill processes
-    anthill = parallel.importProfile('/share/apps/matlab/anthill.settings');
-    parallel.defaultClusterProfile(anthill);
-end
-
-
-% Check if Compiled 
-% ...anisotropic gaussian filter
-if(~exist('anigauss'))
-    fprintf('Compile the anisotropic gauss filtering with \n');
-    fprintf([' mex Dependencies/anigaussm/anigauss_mex.c Dependencies/'...
-              'anigaussm/anigauss.c -output anigauss'])
-end
-% ...word counter
-if(~exist('mexCountWordsIndex'))
-    fprintf('Compile the word counter with \n');
-    fprintf(' mex Dependencies/mexCountWordsIndex.cpp');
-end
-% ...the code of Felzenszwalb and Huttenlocher, IJCV 2004.
-if(~exist('mexFelzenSegmentIndex'))
-    fprintf('Compiling the segmentation algorithm with\n');
-    fprintf([' mex Dependencies/FelzenSegment/mexFelzenSegmentIndex.cpp'...
-             ' -output mexFelzenSegmentIndex;'])
-end
-
 
 % Load image set
 fprintf('---Prepare computation---\n')
@@ -182,20 +146,8 @@ switch dSET_
         error('Not implemented yet.')
 end
 
-% Segmentation - Run
-fprintf('---Starting processing---\n')
-if run_on_anthill  % ...on the cluster
-    cluster_opts.qsubargs = '-l ironfs -l h_rt=02:59:00 -l virtual_free=1G -l mem_free=1G';
-    cluster_opts.paths = {rootPath, [rootPath '/utils'], toolboxPath, genpath(selectivePath)};
-    num_tasks = 100;  % Note: set to 0 for debugging
-    % Run tasks on cluster
-    val = parallelize_function(@extract_segmentation, {imageListClass, ...
-                               imagePath, savePath, seg_params}, ...
-                               [1, 0, 0, 0], num_tasks, cluster_opts);
+
+for i = 1:n_classes
     
-else      % ...on standard PC
-    for i = 1:n_classes 
-        extract_segmentation(imageListClass{i}, imagePath, savePath, seg_params);
-    end
 end
-fprintf('\n');
+
