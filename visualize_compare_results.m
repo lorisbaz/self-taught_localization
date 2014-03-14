2dcb42421afa8124aea856bafde7209dcf545220
Loris Bazzani <lbazzani@anthill.cs.dartmouth.edu>
2014-03-14 17:16:58 -0400

    Slightly modified matlab result script

--- visualize_compare_results.m
+++ visualize_compare_results.m
@@ -5,50 +5,61 @@ clear;
 % init
 i = 0;
 experiments_output_directory = '/home/ironfs/scratch/vlg/Data_projects/grayobfuscation';
+no_GT = 0;
+no_Top5 = 1;
 
-% GT
-% i = i + 1;
-% exp_set(i).name = 'exp08_03';
-% exp_set(i).spec = 'GT label'; 
-% exp_set(i).method = 'GrayBox';
+% *** GT
+if ~no_GT
+    i = i + 1;
+    exp_set(i).name = 'exp08_03';
+    exp_set(i).spec = 'GT label';
+    exp_set(i).method = 'GrayBox';
+    exp_set(i).color = [0.4228    0.7011    0.1280];
+    
+    i = i + 1;
+    exp_set(i).name = 'exp08_05';
+    exp_set(i).spec = 'GT label';
+    exp_set(i).method = 'ClassicSliding';
+    exp_set(i).color = [0.5479    0.6663    0.9991];
+    
+    i = i + 1;
+    exp_set(i).name = 'exp08_07';
+    exp_set(i).spec = 'GT label';
+    exp_set(i).method = 'GraySegm';
+    exp_set(i).color = [0.9427    0.5391    0.1711];
+end
 
-i = i + 1;
-exp_set(i).name = 'exp08_05';
-exp_set(i).spec = 'GT label';
-exp_set(i).method = 'ClassicSliding';
-
-i = i + 1;
-exp_set(i).name = 'exp08_07';
-exp_set(i).spec = 'GT label';
-exp_set(i).method = 'GraySegm';
-
-% TopC
-i = i + 1;
-exp_set(i).name = 'exp08_08';
-exp_set(i).spec = 'Top 5 labels'; 
-exp_set(i).method = 'GraySegm';
-
-i = i + 1;
-exp_set(i).name = 'exp08_09';
-exp_set(i).spec = 'Top 5 labels';
-exp_set(i).method = 'GrayBox';
-
-i = i + 1;
-exp_set(i).name = 'exp08_10';
-exp_set(i).spec = 'Top 5 labels'; 
-exp_set(i).method = 'ClassicSliding';
+% *** Top 5
+if ~no_Top5
+    i = i + 1;
+    exp_set(i).name = 'exp08_08';
+    exp_set(i).spec = 'Top 5 labels';
+    exp_set(i).method = 'GraySegm';
+    exp_set(i).color = [0.4177    0.6981    0.0326];
+    
+    i = i + 1;
+    exp_set(i).name = 'exp08_09';
+    exp_set(i).spec = 'Top 5 labels';
+    exp_set(i).method = 'GrayBox';
+    exp_set(i).color = [0.9831    0.6665    0.5612];
+    
+    i = i + 1;
+    exp_set(i).name = 'exp08_10';
+    exp_set(i).spec = 'Top 5 labels';
+    exp_set(i).method = 'ClassicSliding';
+    exp_set(i).color = [0.3015    0.1781    0.8819];
+end
 
 figure(121);
 hold on;
-colors_rnd = rand(length(exp_set),3);
 legend_stuff = cell(length(exp_set),1);
 for i = 1:length(exp_set)
     load([experiments_output_directory '/' exp_set(i).name ...
-                        '/mat/recall_vs_numPredBboxesImage.mat'])
+        '/mat/recall_vs_numPredBboxesImage.mat'])
     plot(eval(['x_values_' exp_set(i).name]), eval(['recall_' ...
-                exp_set(i).name]),'Color',colors_rnd(i,:), 'LineWidth', 3)
+        exp_set(i).name]),'Color',exp_set(i).color, 'LineWidth', 3)
     legend_stuff{i} =  [exp_set(i).spec ' - ' exp_set(i).method ' (' ...
-                exp_set(i).name ')'];
+        exp_set(i).name ')'];
 end
 hold off;
 grid on;
