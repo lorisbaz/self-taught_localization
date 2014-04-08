34d59e7cb1ed565eb9c2143b26aa6d032d2d66c5
Alessandro Bergamo <aleb@cs.dartmouth.edu>
2014-04-08 16:00:00 -0400

    SelectiveSearch comparisons

--- HierSegmentation_SelSearch/selective_search_pascal_original_boxes.m
+++ HierSegmentation_SelSearch/selective_search_pascal_original_boxes.m
@@ -11,7 +11,7 @@ params.out_file = '/home/anthill/vlg/SelectiveSearchCodeIJCV/demoPascal2007.mat'
 % gt file
 params.gt_bboxes = ['/home/anthill/vlg/SelectiveSearchCodeIJCV/GroundTruthVOC2007test.mat'];
 % max_num_pred_bboxes
-params.max_num_pred_bboxes = [50, 100, 200, 300, 400, 500, 750, 1000, 1500, 2500];
+params.max_num_pred_bboxes = [1:20, 25, 30, 35, 40, 50, 100, 200, 300, 400, 500, 750, 1000, 1500, 2500];
 
 
 % load the pred and gt boxes
@@ -19,7 +19,7 @@ load(params.gt_bboxes);
 load(params.out_file);
 
 
-if 1
+if 0
   mabo_all = [];
   for i=1:numel(params.max_num_pred_bboxes)
     % Get for each ground truth box the best Pascal Overlap Score
@@ -45,6 +45,7 @@ end
 if 1
   recall_mean_all = [];
   for i=1:numel(params.max_num_pred_bboxes)
+    fprintf('num_pred_bboxes: %d/%d\n', i, numel(params.max_num_pred_bboxes));
     % Get for each ground truth box the best Pascal Overlap Score
     max_num_pred_bboxes = params.max_num_pred_bboxes(i);
     % for each gt bbox, get the max overlap
