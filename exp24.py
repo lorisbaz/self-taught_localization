b3e35fa2fdcb9b594f37cc54fff2a45aacc1c722
Loris Bazzani <lbazzani@anthill.cs.dartmouth.edu>
2014-05-20 16:58:17 -0400

    Adding pipeline detection with girshick eval protocol. Adding merging annoobjects function in exp24.

--- exp24.py
+++ exp24.py
@@ -10,6 +10,7 @@ import scipy.misc
 import scipy.io
 import skimage.io
 import tempfile
+import shutil
 from vlg.util.parfun import *
 
 from annotatedimage import *
@@ -20,8 +21,8 @@ class Params:
     def __init__(self):
         # Name of the classifier/method used to generate the results
         self.name_pred_objects = ''
-        # save features to file 
-        self.save_features_cache = True        
+        # save features to file
+        self.save_features_cache = True
         # Max number of buonding boxes that are stored
         self.max_num_bboxes = 1000
         # Usual parameters to run the exp on the cluster
@@ -31,8 +32,6 @@ class Params:
         self.feature_extractor_params = None
 
 def pipeline(inputdb, output_dir, params):
-    # Display info machine for debugging purposes
-    logging.info(os.uname())
     # Instantiate some objects, and open the database
     conf = params.conf
     assert params.feature_extractor_params != None
@@ -44,7 +43,7 @@ def pipeline(inputdb, output_dir, params):
     # loop over the images
     for image_key in db_keys:
         # create a file for each image
-        if ('JPEG' in image_key) or ('png' in image_key): 
+        if ('JPEG' in image_key) or ('png' in image_key):
             pkl_filename = image_key.replace('JPEG','pkl')
         else:
             pkl_filename = image_key + '.pkl'
@@ -53,24 +52,27 @@ def pipeline(inputdb, output_dir, params):
         # get database entry
         anno = pickle.loads(db_input[image_key])
         # get stuff from database entry
-        logging.info('***** Extracting features for ' + \
-                                        os.path.basename(anno.image_name))
+        logging.info('***** Extracting features for {0}, methods {1} '.format(\
+                os.path.basename(anno.image_name), anno.pred_objects.keys()))
         # Register FeatureExtractorParams
         anno.register_feature_extractor(feature_extractor_params, \
                                         params.save_features_cache)
         # Filter the ouput pred bboxes (textout is NOT used!)
         if params.max_num_bboxes>0:
-            textout, pred_objects = anno.export_pred_bboxes_to_text( \
-                params.name_pred_objects, params.max_num_bboxes, \
-                output_filtered_pred_obj = True)
-            anno.pred_objects[params.name_pred_objects] = pred_objects
-        # extract features for pred_objects and GT
-        anno_pred_GT = [anno.pred_objects[params.name_pred_objects], \
-                        anno.gt_objects]
-        for anno_now in anno_pred_GT: 
-            for label in anno_now.keys():
-                anno.extract_features(anno_now[label].bboxes)
-        # adding the AnnotatedImage with the heatmaps to the database 
+            for this_method in anno.pred_objects.keys():
+                textout, pred_objects = anno.export_pred_bboxes_to_text( \
+                            this_method, params.max_num_bboxes, \
+                            output_filtered_pred_obj = True)
+                anno.pred_objects[this_method] = pred_objects
+        # extract features GT
+        for label in anno.gt_objects.keys():
+            anno.extract_features(anno.gt_objects[label].bboxes)
+        # extract features pred_objects from different METHOD(s)
+        for this_method in anno.pred_objects.keys():
+            for label in anno.pred_objects[this_method].keys():
+                anno.extract_features(\
+                                anno.pred_objects[this_method][label].bboxes)
+        # adding the AnnotatedImage with the heatmaps to the database
         logging.info(str(anno))
         logging.info('Save annotated image to {0}'.format(pkl_filename))
         # write pkl representation to file
@@ -78,13 +80,89 @@ def pipeline(inputdb, output_dir, params):
         pkl_file.close()
     return 0
 
+def pipeline_merge(inputdbs, outputdb, params):
+    # Instantiate some objects, and open the database
+    conf = params.conf
+    # retrieve all the AnnotatedImages and images from the database
+    dbs_input = []
+    for inputdb in inputdbs:
+        logging.info('Opening ' + inputdb)
+        dbs_input.append(bsddb.btopen(inputdb, 'r'))
+    db_output = bsddb.btopen(outputdb, 'c')
+    db_keys = dbs_input[0].keys() # keys must be the same for all!
+    # loop over the images
+    for image_key in db_keys:
+        anno_out = []
+        for db_input in dbs_input:
+            # get database entry
+            anno = pickle.loads(db_input[image_key])
+            # merge different anno images
+            if anno_out == []:
+                # init
+                logging.info('***** Merging annotated images for ' + \
+                                        os.path.basename(anno.image_name))
+                anno_out = anno
+            else:
+                # merge the pred objects that are not present
+                for classifier in anno.pred_objects.keys():
+                    if not anno_out.pred_objects.has_key(classifier):
+                        anno_out.pred_objects[classifier] = \
+                                                anno.pred_objects[classifier]
+                    else:
+                        logging.info('Key {0} already present'.\
+                                                        format(classifier))
+        stuff = ['GT']
+        stuff.extend(anno.pred_objects.keys())
+        logging.info('- Classifier {0}'.format(stuff))
+        # adding the AnnotatedImage with the heatmaps to the database
+        logging.info(str(anno_out))
+        db_output[image_key] = pickle.dumps(anno_out, protocol=2)
+    # close all the input dbs
+    for db_input in dbs_input:
+        db_input.close()
+    # save modifications to disk
+    db_output.sync()
+    db_output.close()
+    return 0
 
 def run_exp(params):
     # create output directory
     if os.path.exists(params.output_dir) == False:
         os.makedirs(params.output_dir)
-    # change the protobuf file (for batch mode)
-    filetxt = open(params.conf.ilsvrc2012_caffe_model_spec)
+    # run the pipeline that merges different input dbs
+    tmp_output_dir = params.output_dir + '_TMP'
+    remove_tmp_output_dir = False
+    if isinstance(params.input_dir, list):
+        # create tmp output dir that will be destroyed at the end of the script
+        if os.path.exists(tmp_output_dir) == False:
+            os.makedirs(tmp_output_dir)
+        # list the databases chuncks (use the first, have to be sync!)
+        n_chunks = len(glob.glob(params.input_dir[0] + '/*.db'))
+        # run the pipeline
+        parfun = None
+        if params.run_on_anthill:
+            jobname = 'Job{0}'.format(params.exp_name).replace('exp','')
+            parfun = ParFunAnthill(pipeline_merge, time_requested=23, \
+                                    job_name=jobname + '_merge')
+        else:
+            parfun = ParFunDummy(pipeline_merge)
+        if len(params.task) == 0:
+            idx_to_process = range(n_chunks)
+        else:
+            idx_to_process = params.task
+        for i in idx_to_process:
+            inputdbs = []
+            for input_dir in params.input_dir:
+                inputdbs.append(input_dir + '/%05d'%i + '.db')
+            tmp_outputdb = tmp_output_dir + '/%05d'%i + '.db'
+            parfun.add_task(inputdbs, tmp_outputdb, params)
+        out = parfun.run()
+        for i, val in enumerate(out):
+            if val != 0:
+                logging.info('Task {0} didn''t exit properly'.format(i))
+        params.input_dir = tmp_output_dir
+        remove_tmp_output_dir = True
+
     # list the databases chuncks
     n_chunks = len(glob.glob(params.input_dir + '/*.db'))
     # run the pipeline
@@ -106,5 +184,9 @@ def run_exp(params):
     for i, val in enumerate(out):
         if val != 0:
             logging.info('Task {0} didn''t exit properly'.format(i))
+    # delete tmp output dir (if exists)
+    if remove_tmp_output_dir:
+        logging.info('Removing temp directory {0}'.format(tmp_output_dir))
+        shutil.rmtree(tmp_output_dir + '/')
     logging.info('End of the script')
 
---
+++ exp25_06.py
@@ -0,0 +1,86 @@
+import vlg.util.pbar
+import vlg.util.parfun
+
+from util import *
+from detector import *
+from featextractor import *
+from network import *
+from configuration import *
+from pipeline_detector import *
+
+if __name__ == "__main__":
+    conf = Configuration()
+    # *** FeatureExtractor
+    feature_extractor_params = load_obj_from_file_using_pickle( \
+                                       'featextractor_specs/001.pkl')
+    # *** Detector
+    detector_params = DetectorLinearSVMParams()
+    # *** PipelineDetectorParams
+    params = PipelineDetectorParams()
+    # experiment name
+    params.exp_name = 'exp25_06'
+    # input
+    params.exp_name_input_train = 'exp24_07'
+    params.exp_name_input_test = 'exp24_06'
+    # categories, splits
+    params.categories_file = 'pascal2007/categories.txt'
+    params.splits_dir = 'pascal2007'
+    params.split_train_name = 'trainval'
+    params.split_test_name = 'test'
+    # input/output dirs
+    params.output_dir = conf.experiments_output_directory \
+                        + '/' + params.exp_name
+    params.input_dir_train = conf.experiments_output_directory \
+                        + '/' + params.exp_name_input_train
+    params.input_dir_test = conf.experiments_output_directory \
+                        + '/' + params.exp_name_input_test
+    # FeatureExtractor module to use (parameters object)
+    params.feature_extractor_params = feature_extractor_params
+    # Detector module to use (parameters object)
+    params.detector_params = detector_params
+    # field names for the pos/neg bboxes
+    params.field_name_pos_bboxes = 'GT'
+    params.field_name_bboxes = 'PRED:SELECTIVESEARCH'
+    # neg_bboxes_overlapping_with_pos_params
+    params.neg_bboxes_overlapping_with_pos_params = [0.0, 0.3, 0.3, 0.7]
+    # number of iterations
+    params.num_iterations = 3
+    # evaluation parameters (Girshick's evaluation)
+    params.max_per_image = 100
+    params.max_per_set_factor = 40
+    # visualization
+    params.progress_bar_params = vlg.util.pbar.ProgressBarPlusParams()
+    # ParFun Categories
+    parfun_tmpdir = '/home/ironfs/scratch/vlg/Data_projects/grayobfuscation/TEMP2'
+    if 0:  # -- Anthill
+        params.parfun_params_categories = vlg.util.parfun.ParFunAnthillParams( \
+                        time_requested=10, memory_requested=6, \
+                        progress_bar_params = params.progress_bar_params, \
+                        tmp_dir = parfun_tmpdir, max_tasks=100)
+    if 1:  # -- Local
+        params.parfun_params_categories = vlg.util.parfun.ParFunDummyParams()
+    # ParFun TRAINING
+    if 1:  # -- Anthill
+        params.parfun_params_training = vlg.util.parfun.ParFunAnthillParams( \
+                        time_requested=10, memory_requested=2, \
+                        progress_bar_params = params.progress_bar_params, \
+                        tmp_dir = parfun_tmpdir, max_tasks=1000)
+    if 0:  # -- Local, multi-core
+        params.parfun_params_training = vlg.util.parfun.ParFunProcessesParams( \
+                num_processes = 12, \
+                progress_bar_params = params.progress_bar_params)
+    # ParFun EVALUATION
+    if 1:  # -- Anthill
+        params.parfun_params_evaluation = vlg.util.parfun.ParFunAnthillParams( \
+                        time_requested=10, memory_requested=2, \
+                        progress_bar_params = params.progress_bar_params, \
+                        tmp_dir = parfun_tmpdir, max_tasks=1000)
+    if 0:  # -- Local, multi-core
+        params.parfun_params_evaluation = vlg.util.parfun.ParFunProcessesParams( \
+                num_processes = 12, \
+                progress_bar_params = params.progress_bar_params)
+
+    # run just the first category
+    params.categories_to_process = []
+    # *** run the pipeline
+    PipelineDetector.train_evaluate_detectors(params)
